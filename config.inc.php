65e5884f0c953ec7b79709a225a0410ef57075b2
masterchamp <mahdeep@gmx.de>
2013-01-04 11:37:05 +0100

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-$NAusername = "____EMAIL_ACCOUNT_HERE____";
-$NApwd      = "____PASS_ACCOUNT_HERE____";
+$NAusername = "mahdeep@gmx.de";
+$NApwd      = "master76";
 $NAconfig   = array(
     'client_id'     => '____API_CLIENT_ID_HERE____',
     'client_secret' => '____API_CLIENT_SECRET_HERE____',
